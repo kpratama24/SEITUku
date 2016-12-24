fe542c406b60be6d5b98911ca774f86228f03d44
Kevin Pratama <Kevin Pratama>
2016-12-24 15:17:17 +0700

    Remove unneccessary die() method

--- depositor.php
+++ depositor.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION['id'])) {
 	header("Location: ./");
-} else if ($_SESSION['roleId'] != 1) {
+} else if ($_SESSION['roleId'] != 2) {
 	header("Location: ./");
 }
 
