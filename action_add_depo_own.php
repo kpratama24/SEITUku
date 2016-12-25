06f1b484b95280526ca08de8831aa40ef2d6c695
Kevin Pratama <Kevin Pratama>
2016-12-25 18:43:55 +0700

    Implemented Record Own Deposit (Administrator)

--- action_add_depo_own.php
+++ action_add_depo_own.php
@@ -5,15 +5,22 @@ if (!isset($_SESSION['id'])) {
 }
 
 $dbh = include './dbconn.php';
+$sql = "SELECT username FROM user WHERE id = :id";
+$params = array(':id' => $_SESSION['id']);
+$sth = $dbh->prepare($sql);
+$sth->execute($params);
+
+$row = $sth->fetch(PDO::FETCH_ASSOC);
+
 $sql = "INSERT INTO `deposit` (`student_id`, `deposit_date`, `amount_deposit`) VALUES (:studentID, :date, :amount)";
 $params = array(
-  ':studentID' => $_POST['studentID'],
+  ':studentID' => $row['username'],
   ':date' => $_POST['date'],
   ':amount' => $_POST['amount']
 );
 $sth = $dbh->prepare($sql);
 $sth->execute($params);
 
-header("refresh:1;./add_depo_other.php?depositrecordsuccess");
+header("refresh:1;./add_depo_own.php?depositrecordsuccess");
  ?>
- <h1>Please wait...</h1>
+ <h1> Please wait... </h1>
---
+++ add_depo_own.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+  header("Location: ./");
+}
+include "./header.php";
+
+ ?>
+ <div class="container" style="text-align:center">
+   <header>
+     <h2 class="alt" style="text-align:center">SEITUku</h2>
+     <a href="./administrator.php"><button class="button">Go back to Administrator Main Page </button></a>
+   </header>
+
+   <form action="./action_add_depo_own.php" method="post">
+     <?php if (isset($_GET['depositrecordsuccess'])) echo "<h1> <mark>Deposit Recorded Successfully !</mark> </h1>" ?>
+    Date of deposit : <input type="date" name="date"><br>
+    <input type="number" name="amount" placeholder="Amount of Deposit (Numbers Only !)" style="width:400px"><br>
+    <input type="submit" value="Record Deposit">
+    </form>
+ </div>
+ <?php include "./footer.php" ?>
