85f67fffac30353670646173624cdd2b13a91796
Kevin Pratama <Kevin Pratama>
2016-12-25 19:38:46 +0700

    Add Property text on footer

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@ bottom:0px;
 left:0px;
 right:0px;
 margin-bottom:0px;">
-&copy; SEITU 2017. All rights reserved
+&copy; SEITU 2017. Property of : <a href="http://nearkevin.cu.cc">NearKevin </a>
 </div>
 </body>
 </html>
