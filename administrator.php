f3f5d92562e4965882b4922d9ddaa838958c0353
Kevin Pratama <Kevin Pratama>
2016-12-26 21:25:03 +0700

    modify .htaccess and modifying administrator.php

--- administrator.php
+++ administrator.php
@@ -14,16 +14,49 @@ $sth = $dbh->prepare($sql);
 $sth->execute($params);
 
 $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+$count = 1;
+$total = 0;
+$sqlDeposit = "SELECT deposit_date, amount_deposit FROM deposit WHERE student_id = :id";
+$paramsDeposit = array(':id' => $row['username']);
+$sthDeposit = $dbh->prepare($sqlDeposit);
+$sthDeposit->execute($paramsDeposit);
+
+$rowDeposit = $sthDeposit->fetchall(PDO::FETCH_ASSOC);
 ?>
 <div class="container">
   <header>
     <h2 class="alt" style="text-align:center">SEITUku</h2>
     <p style="text-align:center">Welcome <?php echo $row['username']; ?> ! <a href="./logout.php">Logout</a></p>
   </header>
+	<table>
+    <tr>
+      <td>Number</td>
+      <td>Date</td>
+      <td>Amount</td>
+    </tr>
+    <?php
+      foreach ($rowDeposit as $rowdepo) {
+     ?>
+      <tr>
+        <td><?php echo $count; ?></td>
+        <td><?php echo $rowdepo['deposit_date']; ?></td>
+        <td>IDR <?php echo $rowdepo['amount_deposit']; ?></td>
+      </tr>
+      <?php $count++; ?>
+      <?php }
+      foreach ($rowDeposit as $rowdepo) {
+        $total += $rowdepo['amount_deposit'];
+      }
+      ?>
+      <h2>Deposit Total : IDR <?php echo $total; ?></h2>
+  </table>
+	<hr>
+	<h1>Administrator Section</h1>
   <div style="text-align:center">
     <h2>Search Depositor Data</h2>
     <form action="depositor_search.php" method="get">
-      <input type="text" name="studentID" placeholder="Student ID" style="text-align:center"><br>
+      <input type="text" name="studentID" placeholder="Student ID" style="text-align:center" maxlength="10" required><br>
       <input type="submit" value="Search">
     </form>
     <br>
